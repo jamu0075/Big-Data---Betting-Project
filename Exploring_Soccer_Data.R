6589d41924ad85bb69f3830653dd0b30f9153eac
nickvarberg <nick.varberg@my.wheaton.edu>
2019-03-17 17:00:55 -0600

    use for ML algo, pick you league first

--- Exploring_Soccer_Data.R
+++ Exploring_Soccer_Data.R
@@ -1,5 +1,6 @@
 library(data.table)
 library(dplyr)
+library(tidyr)
 data_path <- "/Users/nickvarberg/Downloads/beat-the-bookie-odds-series-football-dataset/"
 
 ##### change data ####
@@ -15,32 +16,36 @@ series_b_matches <- fread(paste0(data_path, "odds_series_b_matches.csv"))
 matches_all <- full_join(series_matches, series_b_matches)
 series_all <- full_join(series, series_b)
 matches_all <- matches_all %>% filter(match_id %in% series_all$match_id)
+rm(series, series_b, series_matches, series_b_matches)
 
-## premier league tables
-#matches_premier <- matches_all %>% filter(league == "England: Premier League")
-#series_premier  <- series_all %>% filter(match_id %in% matches_premier$match_id)
-#write.csv(series_premier, file = "series_premier_league.csv")
-#write.csv(matches_premier, file = "matches_premier_league.csv")
-# fread to get premier league tables
-
-#### Find best bookkeeper for premier league ####
+#### Find best bookkeeper for all leagues ####
 num = 216
-min = 9999
-for (bookkeeper in c(1:32)){
-  little <- series_premier[,(6+(bookkeeper-1)*num):(6+bookkeeper*num - 1)]
+min = 999999999
+books = c(9)
+for (bookkeeper in books){
+  little <- series[,(6+(bookkeeper-1)*num):(6+bookkeeper*num - 1)]
   num_NaN    <- sum(little == "NaN")
   if (num_NaN < min){
     min = num_NaN
     min_book = bookkeeper
   }
-  if (bookkeeper == 32){print(min_book)}
+  if (bookkeeper == last(books)){ print(min)
+    print(min_book)}
 }
-# bet395 (bookkeeper 9) has the most data
-bookkeeper = 9
-series_premier_bet365 <- series_premier[,c(1:5,(6+(bookkeeper-1)*num):(6+bookkeeper*num - 1))]
-series_premier_bet365
+print(min/(216*31074))
+# Results: min bookkeeper is 7 and has 41% NaN
+#          bookkeeper 9, bet365, has 46% NaN
 
-#### Create some features for premier league bet365####
+##### bet365 table ####
+bookkeeper <- 9
+bet365cols <- c(1:5,(6+(bookkeeper-1)*num):(6+bookkeeper*num - 1))
+bet3651 <- series %>% select(., get("bet365cols"))
+bet3652 <- series_b %>% select(., get("bet365cols"))
+bet365 <- full_join(bet3651, bet3652)
+rm(bet3651,bet3652)
+
+
+#### Create some features for bet365####
 bet365 <- fread("series_premier_league_bet365.csv")
 bet365 <- bet365 %>%
   mutate(outcome = if_else(score_home >= score_away, if_else(score_home == score_away, "draw", "home"), "away")) %>%
@@ -55,7 +60,8 @@ bet365 <- bet365 %>%
   mutate(away_opening_minus_closing = away_opening - away_closing) %>%
   mutate(closing_odds_outcome = if_else(outcome %in% c("home", "draw"), (if_else(outcome == "home", home_closing, draw_closing)), away_closing))
 
-df <- bet365 %>% select(c(7:78))
+# home df
+df <- bet365 %>% select(c(6:77))
 min <- apply(df, 1, FUN = min)
 max <- apply(df, 1, FUN = max)
 range <- max - min
@@ -64,7 +70,8 @@ bet365 <- bet365 %>%
   mutate(home_max = max) %>%
   mutate(home_range = range)
 
-df <- bet365 %>% select(c(79:150))
+# draw df
+df <- bet365 %>% select(c(78:149))
 min <- apply(df, 1, FUN = min)
 max <- apply(df, 1, FUN = max)
 range <- max - min
@@ -73,7 +80,8 @@ bet365 <- bet365 %>%
   mutate(draw_max = max) %>%
   mutate(draw_range = range)
 
-df <- bet365 %>% select(c(151:222))
+# away df
+df <- bet365 %>% select(c(150:221))
 min <- apply(df, 1, FUN = min)
 max <- apply(df, 1, FUN = max)
 range <- max - min
@@ -85,6 +93,21 @@ bet365 <- bet365 %>%
 
 # reorder columns
 bet365_short <- bet365 %>% 
-  select(c(1:4, "outcome", "closing_odds_outcome", (242-18):242))
+  select(c(1:3, "outcome", "closing_odds_outcome", (241-18):241))
 #write.csv(bet365_short, file = "bet365_outcome_features.csv")
 
+# drop NaNs
+bet365_dropped <- bet365_short %>% drop_na()
+
+# add team ids
+matches_all <- matches_all %>% select(match_id, league, home_team, away_team)
+bet365_joined <- left_join(bet365_dropped, matches_all, by = "match_id")
+bet365_joined <- bet365_joined %>% select(match_id, league, home_team, away_team, outcome,closing_odds_outcome, 
+                                          home_opening, home_closing, home_opening_minus_closing, 
+                                          home_min, home_max, home_range,
+                                          draw_opening, draw_closing, draw_opening_minus_closing, 
+                                          draw_min, draw_max, draw_range,
+                                          away_opening, away_closing, away_opening_minus_closing,
+                                          away_min, away_max, away_range)
+
+#write.csv(series_premier, file = "series_premier_league.csv")
