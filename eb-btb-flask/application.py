cf82f24f03463ae2da51d9957da69393bd1e733a
jamu0075 <jamu0075@colorado.edu>
2019-04-11 17:29:16 -0600

    Database connectivity. New dummy data for Leagues.

--- eb-btb-flask/application.py
+++ eb-btb-flask/application.py
@@ -1,6 +1,7 @@
 from flask import Flask, render_template
 import os
 import pymysql
+from flaskext.mysql import MySQL
 
 # EB looks for an 'app' callable by default.
 app = Flask(__name__)
@@ -9,30 +10,62 @@ app = Flask(__name__)
 teams = [
     {
         'name': 'Team 1',
-        'other': 'Something'
+        'other': 'Team 1 info'
     },
     {
         'name': 'Team 2',
-        'other': 'Something Else'
+        'other': 'Team 2 info'
+    },
+    {
+        'name': 'Team 3',
+        'other': 'Team 3 info'
+    },
+    {
+        'name': 'Team 4',
+        'other': 'Team 4 info'
+    }
+]
+
+leagues = [
+    {
+        'name': 'League 1'
+    },
+    {
+        'name': 'League 2'
+    },
+    {
+        'name': 'League 3'
+    },
+    {
+        'name': 'League 4'
+    },
+    {
+        'name': 'League 5'
     }
 ]
 
 @app.route("/")
 @app.route("/home")
 def home():
-    return render_template('home.html', teams=teams)
+    return render_template('home.html', teams=teams, leagues=leagues)
 
 @app.route("/about")
 def about():
-    return render_template('about.html')
+    return render_template('about.html', title="About")
 
-'''
+
+host = '[HOST]'
+port = '[PORT]'
+user = '[USER]'
+password = '[PASSWORD]'
+dbname = '[DBNAME]'
+
 try:
+    print('Attempting to connect...')
     conn = pymysql.connect(host, user=user, port=port, passwd = password, db=dbname)
-    print("Connected...")
+    print("Connected!")
 except:
     print("An error occurred...")
-'''
 
 # run the app.
 if __name__ == "__main__":
