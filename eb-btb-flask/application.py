2f1f7a1a9549dd15649c1ffdcb5376c0ce981795
jamu0075 <jamu0075@colorado.edu>
2019-04-24 17:12:20 -0600

    Dynamic dropdowns for user to narrow team search down by leagues with DB queries. Currently the POST request is not handled.

--- eb-btb-flask/application.py
+++ eb-btb-flask/application.py
@@ -1,4 +1,6 @@
-from flask import Flask, render_template
+from flask import Flask, render_template, request, jsonify
+from flask_wtf import FlaskForm
+from wtforms import SelectField
 import os
 import pymysql
 from flaskext.mysql import MySQL
@@ -6,23 +8,27 @@ from flaskext.mysql import MySQL
 # EB looks for an 'app' callable by default.
 app = Flask(__name__)
 
+#wtf secret key
+SECRET_KEY = os.urandom(32)
+app.config['SECRET_KEY'] = SECRET_KEY
+
 # A database class to use the DB as an object
 class Database:
     def __init__(self):
         host = 'btb-db-instance.cduiw3cccdch.us-east-1.rds.amazonaws.com'
         port = 3306
-        user = ''
-        password = ''
-        db = ''
+        user = 
+        password =
+        db = 'btb-db'
 
         print('Attempting to connect...')
         try:
             self.conn = pymysql.connect(host, user=user, port=port, passwd=password, db=db)
             self.curs = self.conn.cursor()
+            print('Connected!')
         except:
             print('An error occured while attempting to connect to the database.')
 
-        #print('Connected!')
 
     # Return unique leagues from the db
     def get_leagues(self):
@@ -40,17 +46,57 @@ class Database:
 
         return result
 
+    # Return unique teams in a given league from the db
+    def get_teams_in_league(self, league):
+        sql = "SELECT DISTINCT home FROM outcomeFeatures WHERE league = '{}' ORDER BY home".format(league)
+        self.curs.execute(sql)
+        result = self.curs.fetchall()
+
+        return result
+
+# A dynamic Form object for UI
+class Form(FlaskForm):
+    league = SelectField('league', choices=[])
+    team1 = SelectField('team1', choices=[])
+    team2 = SelectField('team2', choices=[])
+
+
+# A database connection instance for global use
+myDB = Database()
+
 # Home page
-@app.route("/")
+@app.route("/", methods=['GET', 'POST'])
 @app.route("/home")
 def home():
 
-    myDB = Database()
-    leagues = myDB.get_leagues()
-    teams = myDB.get_teams()
+    myForm = Form()
+    #Fetch form values from the database
+    myForm.league.choices = [(league[0], league[0]) for league in myDB.get_leagues()]
+    myForm.team1.choices = [(team[0], team[0]) for team in myDB.get_teams()]
+    myForm.team2.choices = [(team[0], team[0]) for team in myDB.get_teams()]
 
-    return render_template('home.html', teams=teams, leagues=leagues)
+    if request.method == 'POST':
+        print('FORM RECIEVED')
+        return '<h1>League: {}, Team1: {}, Team2: {}</h1>'.format(form.league.data, form.team1.data, form.team2.data)
 
+    return render_template('home.html', form=myForm)
+
+#Route to handle dynamic dropdown
+@app.route('/team/<league>')
+def team(league):
+
+    # Get teams from user inputed league
+    teams = myDB.get_teams_in_league(league)
+
+    # Create a list of dictionary objects for dropdown
+    teamList = []
+
+    for team in teams:
+        teamObj = {}
+        teamObj['name'] = team[0]
+        teamList.append(teamObj)
+
+    return jsonify({'teams' : teamList})
 
 # About page
 @app.route("/about")
