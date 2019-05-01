6abb6e1f680469a648b8d3e674fc9dd00ee384bc
ErikJ <erik.k.johnson@colorado.edu>
2019-05-01 14:26:23 -0600

    added updated files from master to erik-dev

--- eb-btb-flask/application.py
+++ eb-btb-flask/application.py
@@ -2,7 +2,7 @@
 
 from flask import Flask, render_template, request, jsonify
 from flask_wtf import FlaskForm
-from wtforms import SelectField
+from wtforms import SelectField, SubmitField
 import os
 import pymysql
 from flaskext.mysql import MySQL
@@ -19,8 +19,8 @@ class Database:
     def __init__(self):
         host = 'btb-db-instance.cduiw3cccdch.us-east-1.rds.amazonaws.com'
         port = 3306
-        user = 
-        password =
+        user = 'masterUser'
+        password = 'supremedbpass2002'
         db = 'btb-db'
 
         print('Attempting to connect...')
@@ -34,7 +34,7 @@ class Database:
 
     # Return unique leagues from the db
     def get_leagues(self):
-        sql = 'SELECT DISTINCT league FROM outcomeFeatures ORDER BY league'
+        sql = 'SELECT DISTINCT league FROM outcomeFeatures2 ORDER BY league'
         self.curs.execute(sql)
         result = self.curs.fetchall()
 
@@ -42,7 +42,7 @@ class Database:
 
     # Return unique teams from the db
     def get_teams(self):
-        sql = 'SELECT DISTINCT home FROM outcomeFeatures ORDER BY home'
+        sql = 'SELECT DISTINCT home_team FROM outcomeFeatures2 ORDER BY home_team'
         self.curs.execute(sql)
         result = self.curs.fetchall()
 
@@ -50,7 +50,16 @@ class Database:
 
     # Return unique teams in a given league from the db
     def get_teams_in_league(self, league):
-        sql = "SELECT DISTINCT home FROM outcomeFeatures WHERE league = '{}' ORDER BY home".format(league)
+        sql = "SELECT DISTINCT home_team FROM outcomeFeatures2 WHERE league = '{}' ORDER BY home_team".format(league)
+        self.curs.execute(sql)
+        result = self.curs.fetchall()
+
+        return result
+
+    def get_teams_record(self, team1, team2):
+        teams = (team1, team2)
+        sql = "SELECT match_date, home_team, away_team, winning_team FROM outcomeFeatures2 WHERE home_team IN {} AND away_team IN {}".format(teams, teams)
+
         self.curs.execute(sql)
         result = self.curs.fetchall()
 
@@ -58,33 +67,38 @@ class Database:
 
 # A dynamic Form object for UI
 class Form(FlaskForm):
-    league = SelectField('league', choices=[])
-    team1 = SelectField('team1', choices=[])
-    team2 = SelectField('team2', choices=[])
+    league = SelectField('League', choices=[])
+    team1 = SelectField('Team 1', choices=[])
+    team2 = SelectField('Team 2', choices=[])
 
+    submit = SubmitField('Submit')
 
+#=========================================================
 # A database connection instance for global use
 myDB = Database()
+#=========================================================
 
 # Home page
 @app.route("/", methods=['GET', 'POST'])
-@app.route("/home")
 def home():
 
-    myForm = Form()
+    myForm = Form(request.form)
     #Fetch form values from the database
     myForm.league.choices = [(league[0], league[0]) for league in myDB.get_leagues()]
     myForm.team1.choices = [(team[0], team[0]) for team in myDB.get_teams()]
     myForm.team2.choices = [(team[0], team[0]) for team in myDB.get_teams()]
 
+    # Handle form POST, update page
     if request.method == 'POST':
-        print('FORM RECIEVED')
-        return '<h1>League: {}, Team1: {}, Team2: {}</h1>'.format(form.league.data, form.team1.data, form.team2.data)
+        teams_record = myDB.get_teams_record(myForm.team1.data, myForm.team2.data)
+
+        return render_template('home.html', form=myForm, league=myForm.league.data, team1=myForm.team1.data, team2=myForm.team2.data, teams_record=teams_record)
 
     return render_template('home.html', form=myForm)
 
+
 #Route to handle dynamic dropdown
-@app.route('/team/<league>')
+@app.route('/<league>')
 def team(league):
 
     # Get teams from user inputed league
