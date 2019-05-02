3e8f0e88e108bacc2f83810bc2d65b130f1cb3ce
nickvarberg <nick.varberg@my.wheaton.edu>
2019-05-01 19:47:20 -0600

    got route handler working for figs, comment out to make work

--- eb-btb-flask/application.py
+++ eb-btb-flask/application.py
@@ -15,6 +15,7 @@ from matplotlib.backends.backend_agg import FigureCanvasAgg as FigureCanvas
 from matplotlib.figure import Figure
 import matplotlib.pyplot as plt
 
+
 # EB looks for an 'app' callable by default.
 app = Flask(__name__)
 
@@ -67,11 +68,17 @@ class Database:
     def get_teams_record(self, team1, team2):
         teams = (team1, team2)
         sql = "SELECT match_date, home_team, away_team, winning_team, home_closing, away_closing FROM outcomeFeatures2 WHERE home_team IN {} AND away_team IN {}".format(teams, teams)
-
         self.curs.execute(sql)
         result = self.curs.fetchall()
-
         return result
+
+    def set_full_team_records(self, team1, team2):
+        sql1 = "SELECT winning_team, closing_odds_outcome FROM outcomeFeatures2 WHERE home_team = '{}' OR away_team = '{}' ORDER BY match_date".format(team1, team1)
+        self.curs.execute(sql1)
+        teams.team1_full_record = self.curs.fetchall()
+        sql2 = "SELECT winning_team, closing_odds_outcome FROM outcomeFeatures2 WHERE home_team = '{}' OR away_team = '{}' ORDER BY match_date".format(team2, team2)
+        self.curs.execute(sql2)
+        teams.team2_full_record = self.curs.fetchall()
 
 # A dynamic Form object for UI
 # This is where Jacob gets the stuff:
@@ -82,9 +89,19 @@ class Form(FlaskForm):
 
     submit = SubmitField('Submit')
 
+# global class
+class Teams:
+    def __init__(self):
+        team_records = []
+        team1_full_record = []
+        team2_full_record = []
+        team1 = ''
+        team2 = ''
+
 #=========================================================
 # A database connection instance for global use
 myDB = Database()
+teams = Teams()
 #=========================================================
 
 # Home page
@@ -103,7 +120,13 @@ def home():
     # then passes to class='container graph" section in home html
     if request.method == 'POST':
         teams_record = myDB.get_teams_record(myForm.team1.data, myForm.team2.data)
-        create_team_records_fig(team_records = teams_record, team1 = myForm.team1.data, team2 = myForm.team2.data)
+        teams.team1 = myForm.team1.data
+        teams.team2 = myForm.team2.data
+        teams.team_records = teams_record
+        print('calling next')
+        myDB.set_full_team_records(teams.team1,  teams.team2)
+        #teams.team1_full_record, teams.team2_full_record = myDB.get_full_team_records(teams.team1,  teams.team2)
+        #create_team_records_fig(team_records = teams.team_records, team1 = teams.team1, team2 = teams.team2)
         return render_template('home.html', form=myForm, league=myForm.league.data, team1=myForm.team1.data, team2=myForm.team2.data,
                                teams_record=teams_record)
 
@@ -137,21 +160,57 @@ def about():
 # Nick create this
 @app.route("/nickdev")
 def nickdev():
-    return render_template('nickdev.html', url = '/static/new_plot.png')
+    return render_template('nickdev.html', title = "Nicks Dev", plot_title = (teams.team1 + " vs. " + teams.team2))
 
 
-## @app.route('/plot.png')
-## def plot_png():
-##     # POST to here. get league, team1, team2. send to create_team_record_fig(in here)
-##     fig = create_team_records_fig()
-##     output = io.BytesIO()
-##     FigureCanvas(fig).print_png(output)
-##     return Response(output.getvalue(), mimetype='image/png')
+@app.route('/team_records')
+def plot_team_records():
+    # POST to here. get league, team1, team2. send to create_team_record_fig(in here)
+    fig = create_team_records_fig(teams.team_records, teams.team1, teams.team2)
+    output = io.BytesIO()
+    FigureCanvas(fig).print_png(output)
+    return Response(output.getvalue(), mimetype='image/png')
+
+@app.route('/performance_team1')
+def plot_performance_team():
+    #fig = performance_of_teams_fig(teams.team1, teams.team1_full_record)
+    fig = create_team_records_fig(teams.team_records, teams.team1, teams.team2)
+    output = io.BytesIO()
+    FigureCanvas(fig).print_png(output)
+    return Response(output.getvalue(), mimetype='image/png')
+
+
+def performance_of_teams_fig(team1, team_full_record):
+    df = pd.DataFrame(data=list(team_records), columns = ['winning_team', 'closing_odds_outcome'])
+    n_games = len(df['winning_team'])
+    size_of_each_bet = 100
+    return_on_bet = (df['winning_team'] == team1)*df['closing_odds_outcome']*size_of_each_bet
+    winnings = np.zeros(n_games)
+    winnings[0] = -size_of_each_bet + return_on_bet.values[0]
+    i = 1
+    while i < n_games:
+        winnings[i] = winnings[i-1] - size_of_each_bet + return_on_bet.values[i]
+        i = i+1
+    x = np.linspace(1, n_games, n_games)
+    plt.plot(x,winnings, c = 'grey')
+    plt.hlines(y=0, xmin=0, xmax = n_games)
+    green = winnings > 0
+    colors = ['']*len(green)
+    for i in range(len(green)):
+        if green[i] == True:
+            colors[i] = 'g'
+        else:
+            colors[i] = 'r'
+    fig = plt.figure(tight_layout = True)
+    fig.add_subplot(1,1,1)
+    plt.scatter(x, winnings, c = colors)
+    plt.close()
+    return fig
 
 def create_team_records_fig(team_records, team1, team2):
     df = pd.DataFrame(data=list(team_records), columns=['match_date', 'home_team', 'away_team', 'winning_team', 'home_closing', 'away_closing']) # need list of tuples instead of tuple of tuples
     print(df)
-    print('1 got past')
+    #print('1 got past')
     n_games = df['home_team'].size
     # x is integers from 1 to n_games for team1 then 1 to n_games for team2
     x = np.concatenate([np.linspace(1, n_games, n_games)] * 2)
@@ -183,7 +242,7 @@ def create_team_records_fig(team_records, team1, team2):
         i = i+1
         print(i)
 
-    fig = plt.figure()
+    fig = plt.figure(tight_layout = True)
     ax1 = fig.add_subplot(1,1,1)
     ax1.scatter(x, y, s=sizes * 1000 * (6 - n_games), c=colors, alpha=0.4)
     ax1.set_xlim([0, n_games + 1])
@@ -201,15 +260,16 @@ def create_team_records_fig(team_records, team1, team2):
     import os
     my_path = os.getcwd()
     url = '/static/new_plot.png'
-    print("3 url: " + url)
-    fig.savefig((my_path+url), bbox_inches='tight')
+    #print("3 url: " + url)
+    #fig.savefig((my_path+url), bbox_inches='tight')
     plt.close()
-    print('4 saved .png and closed plt')
-    name = str(team1) + " vs. "+ str(team2)
-    print((my_path + url))
-    print('2 got to end of create_team_records_fig')
+    return fig
+    #print('4 saved .png and closed plt')
+    #name = str(team1) + " vs. "+ str(team2)
+    #print((my_path + url))
+    #print('2 got to end of create_team_records_fig')
 
-
+    
 
 # run the app.
 if __name__ == "__main__":
